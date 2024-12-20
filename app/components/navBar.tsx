916b6264bba4c69d6a1a787d3df02b67c761f387
Asunt70 <sebaj348@gmail.com>
2024-12-20 17:15:56 -0400

    commit all

---
+++ app/components/navBar.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { useState } from 'react';
+import styles from './navbar.module.css'
+
+export default function NavBar () {
+
+    const [sidebar, setSidebar] = useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
+    return (
+        <>
+        <div className={styles.main}>
+        <nav className={styles.nav}>
+        <ul>
+            <li><a href="#home">HOME</a></li>
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="#projects">PROJECTS</a></li>
+            <li><a href="#contact">CONTACT</a></li>
+        </ul>
+        <div className={styles.buttons}>
+            <button className={styles.btnCv}>CV
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
+            </button>
+            <button className={styles.btnHire}>HIRE ME</button>
+        </div>
+        </nav>
+        <nav className={styles.navMobile}>
+            <button className={styles.showSidebar} onClick={showSidebar}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+            </button>
+        </nav>
+    </div>    
+    <div className={`${styles.sidebar} ${sidebar ? styles.active : ''}`}>
+                <ul>
+            <li><a href="#home">HOME</a></li>
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="#projects">PROJECTS</a></li>
+            <li><a href="#contact">CONTACT</a></li>
+        </ul>
+        <div className={styles.buttons}>
+            <button className={styles.btnCv}>CV
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
+            </button>
+            <button className={styles.btnHire}>HIRE ME</button>
+        </div>
+        </div>
+    </>    
+    )
+}
