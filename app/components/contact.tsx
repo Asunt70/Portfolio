d75b1d68cb6f58edc5fbbc996f88ae32840b19a5
Asunt70 <sebaj348@gmail.com>
2024-12-20 21:19:38 -0400

    changes

---
+++ app/components/contact.tsx
@@ -0,0 +1,17 @@
+import styles from './contact.module.css'
+
+export default function Contact() {
+    return(
+        <div className={styles.main}>
+            <form action="submit">
+            <label htmlFor="email">Name / Company</label>
+            <input type="name" name="name" id="name" placeholder='Name or Company'/>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" placeholder='Your Email' />
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" placeholder='Send me a message'></textarea>
+            <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -25,7 +25,13 @@ export default function NavBar () {
         </nav>
         <nav className={styles.navMobile}>
             <button className={styles.showSidebar} onClick={showSidebar}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+                {!sidebar && (
+            <svg className={styles.menu} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+                )}
+                {sidebar && (
+            <svg className={styles.close} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+
+                )}
             </button>
         </nav>
     </div>    
