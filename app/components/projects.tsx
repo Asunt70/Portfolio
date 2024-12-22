3fffaed8bb9d0c3afa0124f23b58866bd3d3b28c
Asunt70 <sebaj348@gmail.com>
2024-12-21 20:59:55 -0400

    changes

---
+++ app/components/projects.tsx
@@ -0,0 +1,11 @@
+import styles from './projects.module.css'
+
+
+interface Project {
+    title: string,
+    description: string,
+    background: string
+}
+export default function Projects(props:Project) {
+    
+}
